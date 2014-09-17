e585586fc495593104623a22b30c95039610afe2
vpmedia <andras@vpmedia.eu>
2014-09-17 23:25:24 +0200

    #1 Env. setting moved to common build.sh

--- build.sh
+++ build.sh
@@ -6,12 +6,19 @@ export GRADLE_HOME
 ANDROID_HOME=/usr/local/opt/android-sdk
 export ANDROID_HOME
 
+ANDROID_NDK_HOME=/usr/local/opt/android-ndk
+export ANDROID_NDK_HOME
+
+ANDROID_NDK_ROOT=/usr/local/opt/android-ndk
+export ANDROID_NDK_ROOT
+
+NDK_HOME=/usr/local/opt/android-ndk
+export NDK_HOME
+
 AIR_HOME=/usr/local/opt/adobe-air-sdk/libexec
 export AIR_HOME
 
 PATH=$AIR_HOME/bin:$PATH
 export PATH
-
-xctool -project ios/QRZBar.xcodeproj -scheme QRZBar build
 
 ./gradlew
